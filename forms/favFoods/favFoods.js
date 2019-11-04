d3ccfc231fd67c98ad4c95aa29911e53b8d5d5cc
Evan Vecera <ebv74541@creighton.edu>
2019-11-03 22:51:50 -0600

    Initial commit

--- forms/favFoods/favFoods.js
+++ forms/favFoods/favFoods.js
@@ -1,1 +1,16 @@
 let goodFoods = ["pizza","apples","cookies","sushi","pasta","steak"]
+lstFoods.onclick=function(choice){
+  if (typeof(choice) == "object")   // user clicked the control
+    return
+    
+  // otherwise get user selection (second click) which is the index 
+  // of the item chosen
+  NSB.MsgBox(`The choice is ${goodFoods[choice]}`)
+}
+
+btnNextForm.onclick=function(){
+  ChangeForm(dessertVoting)
+}
+
+
+
